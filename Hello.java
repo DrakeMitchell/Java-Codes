078c7f3eed8f9030c5046173de62b31ba97048f2
DrakeMitchell <mitchelld7373@my.uwstout.edu>
2024-09-07 17:15:12 -0500

    Hewwo Worwd

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        String x = "Hello World";
+        String x = "Hello World my name is Drake! :>";
         System.out.println(x);
     }
 }
